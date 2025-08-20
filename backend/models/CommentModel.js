b39d07bff8189323716efdb874aab92649ff5d26
AngelMoreno24 <angel.moreno8424@gmail.com>
2025-08-20 13:54:38 -0700

    created comment model

---
+++ backend/models/CommentModel.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+  issueId: { type: Schema.Types.ObjectId, ref: 'Ticket', required: true },
+  author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  content: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+
+
+const User = mongoose.model('Comment', CommentSchema);
+export default User;
