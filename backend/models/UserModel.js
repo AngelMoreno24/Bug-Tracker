2586564832121c1f5928eed91b25ff3b4e57c8be
AngelMoreno24 <angel.moreno8424@gmail.com>
2025-08-20 14:55:54 -0700

    changed passwordHash to password

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  passwordHash: { type: String },
+  password: { type: String },
   role: { type: String, enum: ['admin'], default: 'admin' }, // global admin only
   createdAt: { type: Date, default: Date.now },
 }, { timestamps: true });
