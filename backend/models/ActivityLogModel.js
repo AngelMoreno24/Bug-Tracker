b05a570e05c92072973537e8d270a11b89538fb7
AngelMoreno24 <angel.moreno8424@gmail.com>
2025-08-20 14:08:08 -0700

    created activity log

---
+++ backend/models/ActivityLogModel.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const ActivityLogSchema = new Schema({
+  ticketId: { type: Schema.Types.ObjectId, ref: "Ticket" },
+  userId: { type: Schema.Types.ObjectId, ref: "User" },
+  action: String, // e.g., "status changed from open → in-progress"
+  timestamp: { type: Date, default: Date.now }
+});
+
+
+const User = mongoose.model('Project', ActivityLogSchema);
+export default User;
