f5efaf34ee70260f253f07dabdcc59541e21624d
AngelMoreno24 <angel.moreno8424@gmail.com>
2025-08-29 21:41:21 -0700

    created updateCompany API

--- backend/controllers/companyModel.js
+++ backend/controllers/companyModel.js
@@ -1,3 +1,28 @@
 import Company from "../models/CompanyModel.js";
-import ProjectMember from "../models/ProjectMemberModel.js";
-import User from "../models/UserModel.js";
+
+// -------------------------
+// Update project info (Manager only)
+// -------------------------
+export const updateCompany = async (req, res) => {
+  try { 
+
+    const membership = await Company.findOne({
+      ownerId: req.user._id,
+    });
+
+    if (!membership || membership.role !== "Admin") {
+      return res.status(403).json({ message: "Only Admin can update the company" });
+    }
+
+    const updatedCompany = await Company.findByIdAndUpdate(
+      ownerId,
+      req.body,
+      { new: true }
+    );
+
+    res.status(200).json({ message: "Company updated", company: updatedCompany });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
