1319b6b872a7c92ceb71897d7101214fb58a4311
AngelMoreno24 <angel.moreno8424@gmail.com>
2025-08-30 14:53:26 -0700

    create get project backend call

--- frontend/src/api/ProjectAPI.jsx
+++ frontend/src/api/ProjectAPI.jsx
@@ -5,18 +5,38 @@ export const createProject = async (title, description) => {
 
     try {
       const res = await axios.post(
-        `${import.meta.env.VITE_BACKEND_URL}/api/projects/create`,
+        `${import.meta.env.VITE_BACKEND_URL}/api/projects/`,
         { title, description },
+        { withCredentials: true }
+      );
+
+
+      // redirect after successful login
+      navigate("/accounts/dashboard", { replace: true });
+
+      return true;
+    } catch (err) {
+      console.error("Login failed:", err.response?.data || err.message);
+      return false;
+    }
+
+
+}
+
+export const getProject = async () => {
+
+
+    try {
+      const res = await axios.get(
+        `${import.meta.env.VITE_BACKEND_URL}/api/projects/`,
         { withCredentials: true }
       );
 
       setToken(res.data.token);
       setUser(res.data.user);
 
-      // redirect after successful login
-      navigate("/accounts/dashboard", { replace: true });
 
-      return true;
+      return res.data;
     } catch (err) {
       console.error("Login failed:", err.response?.data || err.message);
       return false;
