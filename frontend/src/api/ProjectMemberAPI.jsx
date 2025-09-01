683f4e3f852a654f2595d945fdaaae58d46cd095
AngelMoreno24 <angel.moreno8424@gmail.com>
2025-09-01 00:01:58 -0700

    created api call for removeProjectMember and addProjectMember

--- frontend/src/api/ProjectMemberAPI.jsx
+++ frontend/src/api/ProjectMemberAPI.jsx
@@ -3,11 +3,13 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 
+// -------------------------
+// Get the members from a project
+// -------------------------
 export const getProjectMembers = async (projectId, token) => {
   try {
     const res = await axios.get(
-      `${import.meta.env.VITE_BACKEND_URL}/api/projectMembers/`,
-        { projectId },
+      `${import.meta.env.VITE_BACKEND_URL}/api/projectMembers/${projectId}`,
       {
         headers: {
           Authorization: `Bearer ${token}`, // ✅ attach token
@@ -19,6 +21,55 @@ export const getProjectMembers = async (projectId, token) => {
     return res.data;
   } catch (err) {
     console.error("Login failed:", err.response?.data || err.message);
+    return false;
+  }
+};
+
+
+// -------------------------
+// Add a user to the project (Manager only)
+// -------------------------
+export const addProjectMember = async (projectId, userId, role, token) => {
+  try {
+    const res = await axios.post(
+      `${import.meta.env.VITE_BACKEND_URL}/api/projectMembers/${projectId}`,
+      { userId, role }, // body
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        withCredentials: true,
+      }
+    );
+
+    return res.data;
+  } catch (err) {
+    console.error("Add member failed:", err.response?.data || err.message);
+    return false;
+  }
+};
+
+
+// -------------------------
+// Remove a member from the project (Manager only)
+// -------------------------
+
+export const removeProjectMember = async (projectId, userId, token) => {
+  try {
+    const res = await axios.delete(
+      `${import.meta.env.VITE_BACKEND_URL}/api/projectMembers/${projectId}`,
+      { userId }, // body
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        withCredentials: true,
+      }
+    );
+
+    return res.data;
+  } catch (err) {
+    console.error("Add member failed:", err.response?.data || err.message);
     return false;
   }
 };
