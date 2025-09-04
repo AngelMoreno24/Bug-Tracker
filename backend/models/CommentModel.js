c3cad20a22d1365170c8dcb1bb4d52858e9aee0e
AngelMoreno24 <angel.moreno8424@gmail.com>
2025-09-03 20:30:41 -0700

    altered comment model

--- backend/models/CommentModel.js
+++ backend/models/CommentModel.js
@@ -2,11 +2,10 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-  issueId: { type: Schema.Types.ObjectId, ref: 'Ticket', required: true },
+  ticketId: { type: Schema.Types.ObjectId, ref: 'Ticket', required: true },
   author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  content: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
-});
+  content: { type: String, required: true }, 
+}, { timestamps: true });
 
 
 
