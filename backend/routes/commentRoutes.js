7bd36fb48bb5a5c491d046af8ff2b78bf23f4020
AngelMoreno24 <angel.moreno8424@gmail.com>
2025-09-04 19:04:14 -0700

    created comment Routes

---
+++ backend/routes/commentRoutes.js
@@ -0,0 +1,27 @@
+import express from "express";
+import {
+  createComment,
+  getComments,
+  getUserComments,
+  deleteComment,
+} from "../controllers/commentController.js";
+
+import { authenticateToken } from "../middleware/tokenAuthentication.js";
+
+const router = express.Router();
+
+router.use(authenticateToken);
+
+// Create a comment
+router.post("/", createComment);
+
+// Get all comment for a ticket
+router.get("/:ticketId", getComments);
+
+// Get assigned comment for current user
+router.get("/", getUserComments);
+
+// Delete a comment
+router.delete("/:commentId", deleteComment);
+
+export default router;
