b5cf491cd07f3759bf4b2f04e27a502c8db4996d
AngelMoreno24 <angel.moreno8424@gmail.com>
2025-09-11 14:29:28 -0700

    created attachment model

---
+++ backend/models/AttachmentModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const AttachmentSchema = new mongoose.Schema(
+  {
+    ticketId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Ticket",
+      required: true,
+    },
+    uploader: {
+      type: String,
+      required: true,
+    },
+    notes: {
+      type: String,
+      default: "",
+    },
+    file: {
+      type: String, // stored filename (e.g., "169446234-file.png")
+      required: true,
+    },
+    originalName: {
+      type: String, // original uploaded filename
+    },
+    mimeType: {
+      type: String, // for knowing if it's image/pdf/etc
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Attachment", AttachmentSchema);
