fd79f35461058f9d787c2cb9032fe261992b0be6
AngelMoreno24 <angel.moreno8424@gmail.com>
2025-09-14 15:28:38 -0700

    added token check for refresh

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.post("/refresh", refresh);
+router.post("/refresh", authenticateToken, refresh);
 router.post("/logout", logout);
 router.get("/me", authenticateToken, me);
 
